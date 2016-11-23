f4efa98a9fbe9af1083d7b7346c00512a196d40d
Kacper Harabasz <kharabasz@chathamfinancial.eu>
2016-11-23 14:53:07 +0100

    Initial commit of WeatherApi

---
+++ WeatherApi/src/WeatherApi/Controllers/CitiesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        const string API_KEY = "AIzaSyBGAqm1Q8yXTrT4RxXjNZFf40iVXe7xdTo";
+
+        [HttpGet("search")]
+        // GET: api/cities/search?byName
+        public async Task<string> Search([FromQuery]string byName)
+        {
+            return await loadCities(byName);
+        }
+
+        private async Task<string> loadCities(string byName)
+        {
+            using (var client = new HttpClient())
+            {
+                var address = string.Format(
+                    "https://maps.googleapis.com/maps/api/place/autocomplete/json?input={0}&types=(cities)&key={1}",
+                    byName,
+                    API_KEY);
+                var response = await client.GetStringAsync(address).ConfigureAwait(false);
+                return response;
+            }
+        }
+
+        [HttpGet("{placeId}")]
+        public async Task<string> Get(string placeId)
+        {
+            return await loadDataAboutCity(placeId);
+        }
+
+        private async Task<string> loadDataAboutCity(string placeId)
+        {
+            using (var client = new HttpClient())
+            {
+                var address = string.Format(
+                    "https://maps.googleapis.com/maps/api/place/details/json?placeid={0}&key={1}",
+                    placeId,
+                    API_KEY);
+                var response = await client.GetStringAsync(address).ConfigureAwait(false);
+                return response;
+            }
+        }
+    }
+}
---
+++ WeatherApi/src/WeatherApi/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.AddMvc();
+
+            services.AddCors();
+
+            services.Configure<CorsOptions>(options =>
+            {
+                var policy = new CorsPolicy();
+                policy.Headers.Add("*");
+                policy.Methods.Add("*");
+                policy.Origins.Add("*");
+                policy.SupportsCredentials = true;
+
+                options.AddPolicy("AllowAllOrigins", policy);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseCors("AllowAllOrigins");
+
+            app.UseMvc();
+        }
+    }
+}
