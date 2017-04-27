5b768a3c495658f8a2c6a941945cf162020c5a48
Ihor Hadzera <ihor_hadzera@epam.com>
2017-04-28 00:32:01 +0200

    added call to API

--- WeatherApi/src/WeatherApi/wwwroot/src/controllers/homeController.js
+++ WeatherApi/src/WeatherApi/wwwroot/src/controllers/homeController.js
@@ -1,9 +1,43 @@
-﻿class HomeController {
-    constructor(){
-        this.name = "Constructor";
+﻿import "easy-autocomplete";
+import "easy-autocomplete/dist/easy-autocomplete.css";
+
+class HomeController {
+    constructor($scope,$http) {
+        const that = this;
+        this.$http = $http;
+        $scope.$on('$viewContentLoaded', function() {
+            $('#cities-autocomplete').easyAutocomplete({
+                url:function(phrase) {
+                    return `/api/cities/search?byName=${phrase}`;
+                },
+                getValue: function(element) {
+                    return element.structured_formatting.main_text;
+                },
+                listLocation: "predictions",
+                list: {
+                    onChooseEvent: function() {
+                        const placeId = $('#cities-autocomplete').getSelectedItemData().place_id;
+                        that.$http({
+                            method: 'GET',
+                            url: `/api/cities/${placeId}`
+                        }).then(function (selectedCity) {
+                            const location = selectedCity.data.result.geometry.location;
+                            that.getForecast(location.lat, location.lng).then(function(forecast) {
+                                that.forecast = forecast.data;
+                                that.selectedCity = selectedCity.data.result;
+                            });
+                        });
+                    }
+                }
+            });
+        });
     }
-    changeName() {
-        this.name = "ADASDASD";
+
+    getForecast(lat,long) {
+        return this.$http({
+            method: 'GET',
+            url: `/api/forecast?latitude=${lat}&longitude=${long}&source=WORLD_WEATHER`
+        });
     }
 }
 
