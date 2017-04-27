6bd9f18832de0a2028b4ae131affd31087f32a6c
Ihor Hadzera <ihor_hadzera@epam.com>
2017-04-27 14:01:16 +0200

    configured vs for running webpack

--- WeatherApi/src/WeatherApi/webpack.config.js
+++ WeatherApi/src/WeatherApi/webpack.config.js
@@ -1,14 +1,14 @@
-﻿const Webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Path = require('path');
+﻿var Webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var Path = require('path');
 
 module.exports = () => {
     return {
-        context: __dirname + "\\src\\",
+        context: __dirname + "\\wwwroot\\src",
         devtool: 'inline-sourcemap',
         entry: '.\\app.js',
         output: {
-            path: __dirname + "\\dist\\",
+            path: __dirname + "\\wwwroot\\dist",
             filename: 'app.js'
         },
         plugins: [
@@ -24,6 +24,10 @@ module.exports = () => {
                     use: ['style-loader', 'css-loader', 'sass-loader']
                 }
             ]
+        },
+        devServer: {
+            contentBase: Path.resolve(__dirname + '\\wwwroot\\src'),
+            port: 8083
         }
     };
 };
